624e5dcea84e61d24040b17ac86b97826c15453f
AAurelioGN <36158509+alejandro61299@users.noreply.github.com>
2020-05-16 16:50:34 +0200

    Added block scripts, levels,  and new target

--- Solid Breaker/Assets/Scripts/BlocksManager.cs
+++ Solid Breaker/Assets/Scripts/BlocksManager.cs
@@ -1,18 +1,51 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BlocksManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    int currentBlockNum = 0;
+    int currentLevelNum = 0;
+    GameObject currentLevel;
+    public Transform origin;
+    public int initialLevel = 1;
+    public GameObject[] levels;
+    public Material[] materials;
     void Start()
     {
-        
+        currentLevelNum = initialLevel;
+        LoadLevel(currentLevelNum);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (currentLevel == null) return;
+
+
+    }
+
+
+    public void LoadLevel(int i)
+    {
+        if (i > levels.Length && i <= 0) return;
+        currentLevelNum = i - 1;
+        currentLevel = Instantiate(levels[currentLevelNum], origin);
+        Block[] blocks = currentLevel.GetComponentsInChildren<Block>();
+        currentBlockNum = 0;
+
+        foreach ( Block block in blocks) {
+            if (!block.gameObject.isStatic)
+                ++currentBlockNum;
+        }
+    }
+
+    public void UnLoadLevel()
+    {
+        if (currentLevel == null) return;
+        Destroy(currentLevel);
+        currentLevel = null;
+        currentBlockNum = 0;
     }
 }
