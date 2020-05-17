aaf1b3274b992798c5074f6be0b8ab7e34ba573b
peterMcP <36275626+peterMcP@users.noreply.github.com>
2020-05-17 21:34:50 +0200

    added player and ball

---
+++ Solid Breaker/Assets/Scripts/Player/BallBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehaviour : MonoBehaviour
+{
+
+    public float radius = 0.5f;
+    public Vector3 movement = Vector3.zero;
+    public float speed = 5.0f;
+    [SerializeField] private float ray_length = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movement = new Vector3(Random.Range(1, 3), 0.0f, Random.Range(1, 3));
+        Debug.Log(movement);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleCollision();
+        transform.position += movement.normalized * speed * Time.deltaTime;
+    }
+
+    void HandleCollision()
+    {
+        // first shoot little ray
+        RaycastHit hit;
+        if(Physics.SphereCast(transform.position, radius, movement.normalized, out hit, ray_length))
+        {
+            Debug.Log("Ball hit with: " + hit.collider.gameObject.name);
+
+            movement = Vector3.Reflect(movement, hit.normal);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireSphere(transform.position, radius * 0.5f);
+        Gizmos.DrawLine(transform.position, transform.position + movement.normalized * ray_length);
+    }
+}
---
+++ Solid Breaker/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [Header("Core vars")]
+    public float vaus_speed = 9.0f;
+    public float max_pos_limits = 5.0f;
+    [SerializeField] bool can_move = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        HandleMovement();
+
+    }
+
+
+    void HandleMovement()
+    {
+        Vector3 new_pos = transform.position;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            new_pos.x -= vaus_speed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            new_pos.x += vaus_speed * Time.deltaTime;
+        }
+
+        new_pos.x = Mathf.Clamp(new_pos.x, -max_pos_limits, max_pos_limits);
+        transform.position = new_pos;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision with: " + collision.gameObject.name);
+
+
+    }
+}
