94d462b268fc0f412b4230dc5273aca14f163914
peterMcP <36275626+peterMcP@users.noreply.github.com>
2020-05-17 23:14:56 +0200

    added ball collision prediction
    - high speed missing collisions mitigated

--- Solid Breaker/Assets/Scripts/Player/BallBehaviour.cs
+++ Solid Breaker/Assets/Scripts/Player/BallBehaviour.cs
@@ -1,20 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
+public class PredictedCollision
+{
+    public GameObject col_obj;
+    public Vector3 col_point;
+    public Vector3 current_movement;
+    public Vector3 col_normal;
+    public bool predicted = false;
+
+    public void FillCollisionData(Vector3 movement, RaycastHit hit)
+    {
+        current_movement = movement;
+        col_point = hit.point;
+        col_normal = hit.normal;
+        col_obj = hit.collider.gameObject;
+        predicted = true;
+    }
+}
 public class BallBehaviour : MonoBehaviour
 {
 
-    public float radius = 0.5f;
+    public float radius = 0.25f;
     public Vector3 movement = Vector3.zero;
     public float speed = 5.0f;
     [SerializeField] private float ray_length = 0.5f;
+
+    [SerializeField] private PredictedCollision predicted_collision;
+    //[SerializeField] private bool dynamic_col = false;
 
     // Start is called before the first frame update
     void Start()
     {
         movement = new Vector3(Random.Range(1, 3), 0.0f, Random.Range(1, 3));
         Debug.Log(movement);
+        predicted_collision = new PredictedCollision();
     }
 
     // Update is called once per frame
@@ -26,20 +48,49 @@ public class BallBehaviour : MonoBehaviour
 
     void HandleCollision()
     {
+        // check collision prediction
+        SetPrediction();
+        // if we have predicted collision pos, check if we passed and reflect
+        if(predicted_collision.predicted)
+        {
+            float offset_min = 0.2f;
+            float dti = (transform.position - predicted_collision.col_point).magnitude;
+            Debug.Log("Distance to impact: " + dti);
+            if(dti <= radius + offset_min)
+            {
+                movement = Vector3.Reflect(movement, predicted_collision.col_normal);
+                predicted_collision.predicted = false;
+            }
+        }
+    }
+
+    void SetPrediction()
+    {
         // first shoot little ray
         RaycastHit hit;
-        if(Physics.SphereCast(transform.position, radius, movement.normalized, out hit, ray_length))
+        if (Physics.SphereCast(transform.position, radius, movement.normalized, out hit, Mathf.Infinity))
         {
-            Debug.Log("Ball hit with: " + hit.collider.gameObject.name);
-
-            movement = Vector3.Reflect(movement, hit.normal);
+            Debug.Log("Ball prediction hit with: " + hit.collider.gameObject.name);
+            predicted_collision.FillCollisionData(movement, hit);
         }
     }
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.magenta;
-        Gizmos.DrawWireSphere(transform.position, radius * 0.5f);
-        Gizmos.DrawLine(transform.position, transform.position + movement.normalized * ray_length);
+        Gizmos.color = Color.red;
+        // DRAW PREDICTION
+        if(predicted_collision != null)
+            Gizmos.DrawWireSphere(predicted_collision.col_point, radius);
+
+        // DRAW DYNAMIC COLLISION WITH PLAYER
+        RaycastHit hit;
+        if (Physics.SphereCast(transform.position, radius, movement.normalized, out hit, Mathf.Infinity))
+        {
+            if (hit.transform.tag == "Player")
+            {
+                Gizmos.color = Color.magenta;
+                Gizmos.DrawWireSphere(predicted_collision.col_point, radius);
+            }
+        }
     }
 }
