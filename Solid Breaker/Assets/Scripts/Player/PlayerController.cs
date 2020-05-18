46ea78b362140901a5696efe295d22be08732696
peterMcP <36275626+peterMcP@users.noreply.github.com>
2020-05-18 02:51:34 +0200

    working on powerups

--- Solid Breaker/Assets/Scripts/Player/PlayerController.cs
+++ Solid Breaker/Assets/Scripts/Player/PlayerController.cs
@@ -6,24 +6,39 @@ public class PlayerController : MonoBehaviour
 {
 
     [Header("Core vars")]
-    public float vaus_speed = 9.0f;
     public float max_pos_limits = 5.0f;
-    [SerializeField] bool can_move = true;
+    [Header("Gameplay vars")]
+    public float vaus_speed = 9.0f;
+    [SerializeField] PowerUpType active_powerup;
+    [SerializeField]private PowerUpType last_powerup;
+    public int lifes = 5;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        active_powerup = PowerUpType.none;
     }
 
     // Update is called once per frame
     void Update()
     {
-
         HandleMovement();
+        HandlePowerUpActive();
 
+        HandleGodKeys();
     }
 
+    void HandleGodKeys()
+    {
+        for(KeyCode i = KeyCode.Alpha0, n = 0; i < KeyCode.Alpha9; ++i, ++n)
+        {
+            if (Input.GetKeyDown(i))
+            {
+                Instantiate(Resources.Load(System.Enum.GetName(typeof(PowerUpType), n)), new Vector3(0, 0.5f, 0), Quaternion.Euler(0.0f, 0.0f, 90.0f));
+            }
+        }
+    }
 
     void HandleMovement()
     {
@@ -42,10 +57,58 @@ public class PlayerController : MonoBehaviour
         transform.position = new_pos;
     }
 
+    void HandlePowerUpActive()
+    {
+        // handle dynamic powerup behaviours
+        switch (active_powerup)
+        {
+            case PowerUpType.Laser:
+                {
+                    HandleLaserShot();
+                    break;
+                }
+        }
+    }
+
+    void HandleLaserShot()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+        }
+    }
+
+    public void ActivePowerUp(PowerUpType type)
+    {
+        last_powerup = active_powerup;
+        active_powerup = type;
+
+        // handle one shot behaviours
+        switch (active_powerup)
+        {
+            case PowerUpType.Enlarge:
+                {
+                    break;
+                }
+            case PowerUpType.Slow:
+                {
+                    break;
+                }
+            case PowerUpType.Disruption:
+                {
+                    // instantiate 2 new balls
+                    break;
+                }
+            case PowerUpType.ExtraLife:
+                {
+                    ++lifes;
+                    break;
+                }
+        }
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         Debug.Log("Collision with: " + collision.gameObject.name);
-
-
     }
 }
---
+++ Solid Breaker/Assets/Scripts/PowerUp.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PowerUpType
+{
+    none, Laser, Enlarge, Slow, Disruption, ExtraLife, 
+}
+public class PowerUp : MonoBehaviour
+{
+    public PowerUpType type;
+    public float rotateAngleSpeed = 180.0f;
+    public float fallSpeed = 3.0f;
+    [SerializeField] private float radius = 0.25f; // capsule radius
+    [SerializeField] private float width = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        radius = GetComponent<CapsuleCollider>().radius;
+        width = GetComponent<CapsuleCollider>().height;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleCollision();
+        transform.position += new Vector3(0.0f, 0.0f, -fallSpeed * Time.deltaTime);
+        transform.Rotate(0.0f, rotateAngleSpeed * Time.deltaTime, 0.0f);
+    }
+
+    void HandleCollision()
+    {
+        RaycastHit hit;
+        if (Physics.BoxCast(transform.position, new Vector3(width * transform.localScale.y, radius * 0.5f, radius * 0.5f), -Vector3.forward, out hit, Quaternion.identity, 0.1f))
+        {
+            if(hit.collider.gameObject.CompareTag("Player"))
+            {
+                // get player controller script and active power up type
+                PlayerController pctrl = hit.collider.GetComponent<PlayerController>();
+                if (pctrl)
+                    pctrl.ActivePowerUp(type);
+
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(transform.position, transform.position + -Vector3.forward * (radius * 0.5f));
+        //Gizmos.DrawWireSphere(transform.position, (width * 0.5f) * transform.localScale.y);
+        Gizmos.DrawWireCube(transform.position, new Vector3(width * transform.localScale.y, radius, radius));
+    }
+}
